ed595067396e60971216d4692d95dfe55ff6c48b
Kassarapron <B6216023@g.sut.ac.th>
2021-11-04 16:02:11 +0700

    init

---
+++ entity/user.go
@@ -0,0 +1,27 @@
+package entity
+
+
+import (
+
+  "time"
+
+  "gorm.io/gorm"
+
+)
+
+
+type User struct {
+
+  gorm.Model
+
+  FirstName    string
+
+  LastName     string
+
+  Email        string
+
+  Age          uint8
+
+  BirthDay     time.Time
+
+}
---
+++ setup.go
@@ -0,0 +1,41 @@
+package entity
+
+
+import (
+
+        "gorm.io/gorm"
+
+        "gorm.io/driver/sqlite"
+
+)
+
+
+var db *gorm.DB
+
+
+func DB() *gorm.DB {
+
+        return db
+
+}
+
+
+func SetupDatabase() {
+
+  database, err := gorm.Open(sqlite.Open("newproject"), &gorm.Config{})
+
+  if err != nil {
+
+    panic("failed to connect database")
+
+  }
+
+
+  // Migrate the schema
+
+  database.AutoMigrate(&User{})
+
+
+  db = database
+
+}
